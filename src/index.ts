bba4fb23e5537128fdf97a7596400e6d1fe97a63
Daniel Fayemi <daniel.fayemi@latoken.com>
2022-07-17 15:44:36 +0100

    addidtional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+ interface Color {
+    red: number;
+    green: number;
+    blue: number;
+}
+
+const color : Color = {
+    red: 0,
+    green: 0,
+    blue: 0
+}
+
+console.log(color)
+
+export default color;
