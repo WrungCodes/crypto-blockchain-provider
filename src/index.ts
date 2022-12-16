0c5a7da9ca74bca2632d296d7f5044170c7128c4
Daniel Fayemi <daniel.fayemi@latoken.com>
2022-12-16 20:16:16 +0100

    added usage to getCryptoProvider

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,29 @@ import { ICryptoProvider } from "./interfaces/ICryptoProvider";
 import { ProviderOptionsType } from "./interfaces/IProviderOptionsType";
 import * as Providers from "./providers";
 import { IProvider } from "./interfaces/IProvider";
+import { ProviderTxOptions, TransactonData } from "./interfaces/CyptoTransaction";
 
 export { IProvider }
 
-export async function getCryptoProvider([provider, options ]: ProviderOptionsType) : Promise<ICryptoProvider<any, any>> 
+export async function getCryptoProvider([provider, options ]: ProviderOptionsType) : Promise<ICryptoProvider<ProviderTxOptions, TransactonData>> 
 { 
     return new Providers[provider](options);
 }
+
+// # Usage of function
+
+// const provider = await getCryptoProvider([ IProvider.ETH, {rpcAddress: 'http://eth.rpc.com'}])
+
+// const { address, privateKey } = await provider.generateKeyPair()
+
+// const balance = await provider.balanceByAddress(address)
+
+// const data = await provider.txCreate({
+//     subtractFee: false,
+//     agent: '',
+//     value: '',
+//     credentials: ''
+    
+// })
+
+// const data = await provider.txTransferRaw({ data: '', info: {} });
